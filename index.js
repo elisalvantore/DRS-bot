d94b036797ea2dd64015833e39e0b452cca46fb9
elisalvantore <akitaarmy@gmail.com>
2025-10-09 14:01:14 +0700

    updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const client = new Client({
     ]
 });
 
-client.once("ready", () => {
+client.once("clientReady", () => {
     console.log(`✅ Bot đã đăng nhập thành công với tên: ${client.user.tag}`);
     client.user.setActivity("Đang chơi PUBG cùng DRS! ❤️", { type: "PLAYING" });
 });
